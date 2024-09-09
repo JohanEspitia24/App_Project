4b6dc02a13e266a085417c0db1893cbf99f5f451
JohanEspitia24 <71570769+JohanEspitia24@users.noreply.github.com>
2024-09-08 19:37:44 -0500

    Submit the comands

---
+++ ProyectoEntrega2/app/src/main/java/com/example/proyectoentrega2/LoginActivity.kt
@@ -0,0 +1,4 @@
+package com.example.proyectoentrega2
+
+class LoginActivity {
+}
---
+++ ProyectoEntrega2/app/src/main/java/com/example/proyectoentrega2/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.proyectoentrega2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+    import android.content.Intent
+
+    import android.os.Handler
+    import android.os.Looper
+
+    class MainActivity : AppCompatActivity() {
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+
+            // Retardo de 3 segundos antes de pasar a la pantalla principal
+            Handler(Looper.getMainLooper()).postDelayed({
+                // Iniciar la actividad principal (MainActivity)
+                val intent = Intent(this, SecondActivity::class.java)
+                startActivity(intent)
+                finish() // Finaliza la SplashActivity para no volver atrás
+            }, 3000) // 3 segundos de retardo
+        }
+    }
